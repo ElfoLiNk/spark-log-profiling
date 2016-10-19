d5cc6594dcf5b45b9b835c1b2dd2e743024ecf18
Matteo Gazzetta <matteo_gazzetta@hotmail.it>
2016-10-19 15:49:32 +0200

    added hybrid heuristic for weight (using duration) and compute nominal rate

--- processing.py
+++ processing.py
@@ -1,8 +1,10 @@
 import glob
-import json
 import re
 from collections import OrderedDict
-from collections import deque
+
+import numpy as np
+
+import json
 
 for log in glob.glob("./logs/app-*"):
     appname = ""
@@ -20,25 +22,60 @@ for log in glob.glob("./logs/app-*"):
                     stage = data["Stage Info"]
                     if stage["Stage ID"] not in stageDict.keys():
                         stageDict[stage["Stage ID"]] = {}
-                        #if stage["Stage ID"] == 0:
-                           # stageDict[0]["totalduration"] = 0
-                        stageDict[stage["Stage ID"]]["genstage"] = True if len(stageDict) == 1 else False
+                        if stage["Stage ID"] == 0:
+                            stageDict[0]["totalduration"] = 0
+                        stageDict[stage["Stage ID"]]["name"] = stage['Stage Name']
+                        # TODO stageDict[stage["Stage ID"]]["genstage"] = True if len(stageDict) == 1 else False
+                        stageDict[stage["Stage ID"]]["genstage"] = False
                         stageDict[stage["Stage ID"]]["parentsIds"] = stage["Parent IDs"]
                         stageDict[stage["Stage ID"]]["nominalrate"] = 0.0
                         stageDict[stage["Stage ID"]]["weight"] = 0
                         stageDict[stage["Stage ID"]]["RDDIds"] = [x["RDD ID"] for x in stage["RDD Info"]]
                         stageDict[stage["Stage ID"]]["skipped"] = False
                         stageDict[stage["Stage ID"]]["cachedRDDs"] = []
+                        stageDict[stage["Stage ID"]]["numtask"] = 0
                         for x in stage["RDD Info"]:
                             storage_level = x["Storage Level"]
-                            if storage_level["Use Disk"] or storage_level["Use Memory"] or storage_level["Deserialized"]:
+                            if storage_level["Use Disk"] or storage_level["Use Memory"] or storage_level[
+                                "Deserialized"]:
                                 stageDict[stage["Stage ID"]]["cachedRDDs"].append(x["RDD ID"])
-
-                # if data["Event"] == "SparkListenerStageCompleted":
-                #     stageDict[stage["Stage ID"]]["duration"] = stage["Completion Time"] - stage["Submission Time"]
-                #     stageDict[0]["totalduration"] += stage["Completion Time"] - stage["Submission Time"]
+                if data["Event"] == "SparkListenerStageCompleted":
+                    # print(data)
+                    stageDict[data["Stage Info"]["Stage ID"]]["numtask"] = data["Stage Info"]['Number of Tasks']
+                    for acc in data["Stage Info"]["Accumulables"]:
+                        if acc["Name"] == "internal.metrics.executorRunTime":
+                            stageDict[data["Stage Info"]["Stage ID"]]["duration"] = acc["Value"]
+                            stageDict[0]["totalduration"] += acc["Value"]
+                        if acc["Name"] == "internal.metrics.input.recordsRead":
+                            stageDict[data["Stage Info"]["Stage ID"]]["recordsread"] = acc["Value"]
+                        else:
+                            try:
+                                stageDict[data["Stage Info"]["Stage ID"]]["recordsread"]
+                            except KeyError:
+                                stageDict[data["Stage Info"]["Stage ID"]]["recordsread"] = 0.0
+                        if acc["Name"] == "internal.metrics.shuffle.read.recordsRead":
+                            stageDict[data["Stage Info"]["Stage ID"]]["shufflerecordsread"] = acc["Value"]
+                        else:
+                            try:
+                                stageDict[data["Stage Info"]["Stage ID"]]["shufflerecordsread"]
+                            except KeyError:
+                                stageDict[data["Stage Info"]["Stage ID"]]["shufflerecordsread"] = 0.0
+                        if acc["Name"] == "internal.metrics.output.recordsWrite":
+                            stageDict[data["Stage Info"]["Stage ID"]]["recordswrite"] = acc["Value"]
+                        else:
+                            try:
+                                stageDict[data["Stage Info"]["Stage ID"]]["recordswrite"]
+                            except KeyError:
+                                stageDict[data["Stage Info"]["Stage ID"]]["recordswrite"] = 0.0
+                        if acc["Name"] == "internal.metrics.shuffle.write.recordsWritten":
+                            stageDict[data["Stage Info"]["Stage ID"]]["shufflerecordswrite"] = acc["Value"]
+                        else:
+                            try:
+                                stageDict[data["Stage Info"]["Stage ID"]]["shufflerecordswrite"]
+                            except KeyError:
+                                stageDict[data["Stage Info"]["Stage ID"]]["shufflerecordswrite"] = 0.0
             except KeyError:
-                None
+                print(data)
 
     skipped = []
     with open(log) as logfile:
@@ -50,12 +87,11 @@ for log in glob.glob("./logs/app-*"):
                         # print(stage)
                         if stage["Stage ID"] not in stageDict.keys():
                             stageDict[stage["Stage ID"]] = {}
-                            #if stage["Stage ID"] == 0:
-                               # stageDict[0]["totalduration"] = 0
                             stageDict[stage["Stage ID"]]["genstage"] = True if len(stageDict) == 1 else False
                             stageDict[stage["Stage ID"]]["parentsIds"] = stage["Parent IDs"]
                             stageDict[stage["Stage ID"]]["nominalrate"] = 0.0
                             stageDict[stage["Stage ID"]]["weight"] = 0
+                            stageDict[stage["Stage ID"]]["duration"] = 0
                             stageDict[stage["Stage ID"]]["RDDIds"] = [x["RDD ID"] for x in stage["RDD Info"]]
                             stageDict[stage["Stage ID"]]["skipped"] = True
                             stageDict[stage["Stage ID"]]["cachedRDDs"] = []
@@ -65,9 +101,6 @@ for log in glob.glob("./logs/app-*"):
                                     "Deserialized"]:
                                     stageDict[stage["Stage ID"]]["cachedRDDs"].append(x["RDD ID"])
                             skipped.append(stage["Stage ID"])
-                # if data["Event"] == "SparkListenerStageCompleted":
-                #     stageDict[stage["Stage ID"]]["duration"] = stage["Completion Time"] - stage["Submission Time"]
-                #     stageDict[0]["totalduration"] += stage["Completion Time"] - stage["Submission Time"]
             except KeyError:
                 None
 
@@ -113,25 +146,47 @@ for log in glob.glob("./logs/app-*"):
     #     i += 1
     # print(i, stage_repeated)
 
-    def setWeight(key):
-        for parentid in stageDict[key]['parentsIds']:
-            w1 = stageDict[key]["weight"] + 1
-            w2 = stageDict[parentid]["weight"]
-            stageDict[parentid]["weight"] = max(w1, w2)
-            setWeight(parentid)
+    # def setWeight(key):
+    #     for parentid in stageDict[key]['parentsIds']:
+    #         w1 = stageDict[key]["weight"] + 1
+    #         w2 = stageDict[parentid]["weight"]
+    #         stageDict[parentid]["weight"] = max(w1, w2)
+    #         setWeight(parentid)
+    #
+    # # Set weights
+    # for key in reversed(stageDict.keys()):
+    #     setWeight(key)
 
-    # Set weights
-    for key in reversed(stageDict.keys()):
-        setWeight(key)
+    stageToDo = len(list(stageDict.keys())) - len(skipped)
+    for stageId in sorted(stageDict.keys()):
+        parentOutput = 0
+        parentInput = 0
+        if stageId not in skipped:
+            stageDict[stageId]["weight"] = stageToDo
+            stageToDo -= 1
+            for parentID in stageDict[stageId]["parentsIds"]:
+                parentOutput += stageDict[parentID]["recordswrite"]
+                parentOutput += stageDict[parentID]["shufflerecordswrite"]
+                parentInput += stageDict[parentID]["recordsread"]
+                parentInput += stageDict[parentID]["shufflerecordsread"]
+            if parentOutput != 0:
+                stageDict[stageId]["nominalrate"] = parentOutput / (stageDict[stageId]["duration"] / 1000.0)
+            elif parentInput != 0:
+                stageDict[stageId]["nominalrate"] = parentInput / (stageDict[stageId]["duration"] / 1000.0)
+            else:
+                stageInput = stageDict[stageId]["recordsread"] + stageDict[stageId]["shufflerecordsread"]
+                if stageInput != 0 and stageInput != stageDict[stageId]["numtask"]:
+                    stageDict[stageId]["nominalrate"] = stageInput / (stageDict[stageId]["duration"] / 1000.0)
+                else:
+                    stageOutput = stageDict[stageId]["recordswrite"] + stageDict[stageId]["shufflerecordswrite"]
+                    stageDict[stageId]["nominalrate"] = stageInput / (stageDict[stageId]["duration"] / 1000.0)
 
-    # for key in stageDict.keys():
-    #     try:
-    #         print("SID " + str(key) + " " + str(stageDict[key]["duration"] / stageDict[0]["totalduration"]))
-    #         oldWeight = stageDict[key]["weight"]
-    #         stageDict[key]["weight"] = stageDict[key]["weight"] / (stageDict[key]["duration"] / stageDict[0]["totalduration"])
-    #         print(oldWeight, stageDict[key]["weight"])
-    #     except KeyError:
-    #         None
+    totalduration = stageDict[0]["totalduration"]
+    for key in stageDict.keys():
+        if key not in skipped:
+            oldWeight = stageDict[key]["weight"]
+            stageDict[key]["weight"] = np.mean([oldWeight, totalduration / stageDict[key]["duration"]])
+            totalduration -= stageDict[key]["duration"]
 
     # Create json output
     with open("./json/" + re.sub("[^a-zA-Z0-9.-]", "_", appname) + ".json", "w") as jsonoutput:
