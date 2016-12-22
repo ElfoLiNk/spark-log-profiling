737aec3ec36708b73f1259f95c604812fba1be60
Matteo Gazzetta <matteo_gazzetta@hotmail.it>
2016-12-22 10:37:21 +0100

    Refactoring

--- processing.py
+++ processing.py
@@ -1,15 +1,14 @@
 import bz2
 import glob
+import json
 import re
 from collections import OrderedDict
 
 import numpy as np
 
-import json
-
 
 def main():
-    for log in glob.glob("./logs/app-*"):
+    for log in glob.glob("./input_logs/app-*"):
         app_name = ""
         # Build stage dictionary
         stage_dict = OrderedDict()
@@ -27,7 +26,7 @@ def main():
                 try:
                     if data["Event"] == "SparkListenerApplicationStart":
                         app_name = data["App Name"]
-                    if data["Event"] == "SparkListenerStageSubmitted":
+                    elif data["Event"] == "SparkListenerStageSubmitted":
                         # print(data)
                         stage = data["Stage Info"]
                         stage_id = stage["Stage ID"]
@@ -41,7 +40,8 @@ def main():
                             stage_dict[stage_id]["nominalrate"] = 0.0
                             stage_dict[stage_id]["weight"] = 0
                             stage_dict[stage_id]["RDDIds"] = {
-                                x["RDD ID"]: {"name": x["Name"], "callsite": x["Callsite"]} for x in stage["RDD Info"]}
+                                x["RDD ID"]: {"name": x["Name"], "callsite": x["Callsite"]} for x in
+                                stage["RDD Info"]}
                             stage_dict[stage_id]["skipped"] = False
                             stage_dict[stage_id]["cachedRDDs"] = []
                             stage_dict[stage_id]["numtask"] = 0
@@ -51,24 +51,25 @@ def main():
                             stage_dict[stage_id]["shufflerecordswrite"] = 0.0
                             for rdd_info in stage["RDD Info"]:
                                 storage_level = rdd_info["Storage Level"]
-                                if storage_level["Use Disk"] or storage_level["Use Memory"] or storage_level[
-                                    "Deserialized"]:
+                                if storage_level["Use Disk"] or storage_level["Use Memory"] or \
+                                        storage_level["Deserialized"]:
                                     stage_dict[stage_id]["cachedRDDs"].append(rdd_info["RDD ID"])
-                    if data["Event"] == "SparkListenerStageCompleted":
+                    elif data["Event"] == "SparkListenerStageCompleted":
                         # print(data)
-                        stage_dict[data["Stage Info"]["Stage ID"]]["numtask"] = data["Stage Info"]['Number of Tasks']
+                        stage_id = data["Stage Info"]["Stage ID"]
+                        stage_dict[stage_id]["numtask"] = data["Stage Info"]['Number of Tasks']
                         for acc in data["Stage Info"]["Accumulables"]:
                             if acc["Name"] == "internal.metrics.executorRunTime":
-                                stage_dict[data["Stage Info"]["Stage ID"]]["duration"] = int(acc["Value"])
+                                stage_dict[stage_id]["duration"] = int(acc["Value"])
                                 stage_dict[0]["totalduration"] += int(acc["Value"])
                             if acc["Name"] == "internal.metrics.input.recordsRead":
-                                stage_dict[data["Stage Info"]["Stage ID"]]["recordsread"] = acc["Value"]
+                                stage_dict[stage_id]["recordsread"] = acc["Value"]
                             if acc["Name"] == "internal.metrics.shuffle.read.recordsRead":
-                                stage_dict[data["Stage Info"]["Stage ID"]]["shufflerecordsread"] = acc["Value"]
+                                stage_dict[stage_id]["shufflerecordsread"] = acc["Value"]
                             if acc["Name"] == "internal.metrics.output.recordsWrite":
-                                stage_dict[data["Stage Info"]["Stage ID"]]["recordswrite"] = acc["Value"]
+                                stage_dict[stage_id]["recordswrite"] = acc["Value"]
                             if acc["Name"] == "internal.metrics.shuffle.write.recordsWritten":
-                                stage_dict[data["Stage Info"]["Stage ID"]]["shufflerecordswrite"] = acc["Value"]
+                                stage_dict[stage_id]["shufflerecordswrite"] = acc["Value"]
                 except KeyError:
                     print(data)
 
@@ -95,7 +96,8 @@ def main():
                                 stage_dict[stage_id]["nominalrate"] = 0.0
                                 stage_dict[stage_id]["weight"] = 0
                                 stage_dict[stage_id]["RDDIds"] = {
-                                    x["RDD ID"]: {"name": x["Name"], "callsite": x["Callsite"]} for x in
+                                    x["RDD ID"]: {"name": x["Name"], "callsite": x["Callsite"]} for
+                                    x in
                                     stage["RDD Info"]}
                                 stage_dict[stage_id]["skipped"] = True
                                 stage_dict[stage_id]["cachedRDDs"] = []
@@ -106,9 +108,10 @@ def main():
                                 stage_dict[stage_id]["shufflerecordswrite"] = 0.0
                                 for rdd_info in stage["RDD Info"]:
                                     storage_level = rdd_info["Storage Level"]
-                                    if storage_level["Use Disk"] or storage_level["Use Memory"] or storage_level[
-                                        "Deserialized"]:
-                                        stage_dict[stage_id]["cachedRDDs"].append(rdd_info["RDD ID"])
+                                    if storage_level["Use Disk"] or storage_level["Use Memory"] or \
+                                            storage_level["Deserialized"]:
+                                        stage_dict[stage_id]["cachedRDDs"].append(
+                                            rdd_info["RDD ID"])
                                 skipped.append(stage_id)
                 except KeyError:
                     None
@@ -116,7 +119,8 @@ def main():
         # Replace skipped stage id in parents ids based on RDD IDs
         for skipped_id in skipped:
             for stage_id1 in stage_dict.keys():
-                if stage_id1 != skipped_id and stage_dict[skipped_id]["RDDIds"] == stage_dict[stage_id1]["RDDIds"]:
+                if stage_id1 != skipped_id and stage_dict[skipped_id]["RDDIds"] == \
+                        stage_dict[stage_id1]["RDDIds"]:
                     for stage_id2 in stage_dict.keys():
                         if skipped_id in stage_dict[stage_id2]["parentsIds"]:
                             stage_dict[stage_id2]["parentsIds"].remove(skipped_id)
@@ -179,17 +183,22 @@ def main():
                     parent_input += stage_dict[parent_id]["recordsread"]
                     parent_input += stage_dict[parent_id]["shufflerecordsread"]
                 if parent_output != 0:
-                    stage_dict[stage_id]["nominalrate"] = parent_output / (stage_dict[stage_id]["duration"] / 1000.0)
+                    stage_dict[stage_id]["nominalrate"] = parent_output / (
+                        stage_dict[stage_id]["duration"] / 1000.0)
                 elif parent_input != 0:
-                    stage_dict[stage_id]["nominalrate"] = parent_input / (stage_dict[stage_id]["duration"] / 1000.0)
+                    stage_dict[stage_id]["nominalrate"] = parent_input / (
+                        stage_dict[stage_id]["duration"] / 1000.0)
                 else:
-                    stage_input = stage_dict[stage_id]["recordsread"] + stage_dict[stage_id]["shufflerecordsread"]
+                    stage_input = stage_dict[stage_id]["recordsread"] + stage_dict[stage_id][
+                        "shufflerecordsread"]
                     if stage_input != 0 and stage_input != stage_dict[stage_id]["numtask"]:
-                        stage_dict[stage_id]["nominalrate"] = stage_input / (stage_dict[stage_id]["duration"] / 1000.0)
+                        stage_dict[stage_id]["nominalrate"] = stage_input / (
+                            stage_dict[stage_id]["duration"] / 1000.0)
                     else:
                         stage_output = stage_dict[stage_id]["recordswrite"] + stage_dict[stage_id][
                             "shufflerecordswrite"]
-                        stage_dict[stage_id]["nominalrate"] = stage_input / (stage_dict[stage_id]["duration"] / 1000.0)
+                        stage_dict[stage_id]["nominalrate"] = stage_input / (
+                            stage_dict[stage_id]["duration"] / 1000.0)
                 if stage_dict[stage_id]["nominalrate"] == 0.0:
                     stage_dict[stage_id]["genstage"] = True
 
@@ -197,11 +206,13 @@ def main():
         for key in stage_dict.keys():
             if key not in skipped:
                 old_weight = stage_dict[key]["weight"]
-                stage_dict[key]["weight"] = np.mean([old_weight, totalduration / stage_dict[key]["duration"]])
+                stage_dict[key]["weight"] = np.mean(
+                    [old_weight, totalduration / stage_dict[key]["duration"]])
                 totalduration -= stage_dict[key]["duration"]
 
         # Create json output
-        with open("./json/" + re.sub("[^a-zA-Z0-9.-]", "_", app_name) + ".json", "w") as jsonoutput:
+        with open("./output_json/" + re.sub("[^a-zA-Z0-9.-]", "_", app_name) + ".json",
+                  "w") as jsonoutput:
             json.dump(stage_dict, jsonoutput, indent=4, sort_keys=True)
 
 
